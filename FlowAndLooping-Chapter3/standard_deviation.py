cf5ecf9b7a20e0c6b06c0fea659c69affb6fea28
kamilkhan@rediffmail.com <donkhan>
2020-01-08 20:00:37 +0530

    code for standard deviation

---
+++ FlowAndLooping-Chapter3/standard_deviation.py
@@ -0,0 +1,22 @@
+import numpy
+import math
+
+def find_std(raw_values):
+    x_bar = 0
+    total = 0
+
+    for val in raw_values:
+        total += val
+
+    x_bar = total/len(raw_values)
+    sigma_total = 0.0
+
+    for val in raw_values:
+        diff = val - x_bar
+        sigma_total += diff * diff
+
+    print(math.sqrt(sigma_total/len(raw_values)))
+    print numpy.std(numpy.array(raw_values))
+
+find_std([6,7,10,11,11,13,16,18,25])
+find_std([74, 72, 83, 96, 64, 79, 88, 69])
