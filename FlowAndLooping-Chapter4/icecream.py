35e21bd9dab0b6fd9551c819773923772bfb2af5
kamilkhan@rediffmail.com <donkhan>
2020-01-17 00:10:49 +0530

    ice cream problem

---
+++ FlowAndLooping-Chapter4/icecream.py
@@ -0,0 +1,9 @@
+
+n = 0
+for i in range(0,11):
+    for j in range(0,11):
+        for k in range(0,11):
+            if i + j + k == 10:
+                print(i,  j ,k)
+                n += 1
+print(n)
