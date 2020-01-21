9d27bdc1c4267abc30dfc6826381a6f427f08abf
donkhan <kamilkhan@rediffmail.com>
2020-01-21 12:17:32 +0530

    Test code for Translation

---
+++ Strings-Chapter5/Ex7.py
@@ -0,0 +1,11 @@
+firstString = "abc"
+secondString = "ghi"
+thirdString = "ab"
+
+string = "abcdef"
+print("Original string:", string)
+
+translation = string.maketrans(firstString, secondString)
+# translate string
+print("Translated string:", string.translate(translation))
+
