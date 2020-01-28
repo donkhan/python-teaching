880c5aebf6ed246e00c0ae81e2406e14a891399a
kkhan <kamilkhan@rediffmail.com>
2020-01-28 22:34:58 +0530

    tutorial code

--- Dictionary-Chapter7/Ex_11.py
+++ Dictionary-Chapter7/Ex_11.py
@@ -1,11 +1,9 @@
-l = ['a','b','a','c','a','b','d']
+list = ['a','b','a','c','a','b','d']
+dictionary = {}
 
-d = {}
-for i in l:
-    if i in d:
-        d[i] = d[i] + 1
+for element in list:
+    if element in dictionary:
+        dictionary[element] = dictionary[element] + 1
     else:
-        d[i] = 1
-
-
-print(d)
+        dictionary[element] = 1
+print(dictionary)
